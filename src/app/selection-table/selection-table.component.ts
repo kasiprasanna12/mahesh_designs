0e232ea4b6b57aee0a919c48df99061fff321185
mahesh <maheshg0610@gmail.com>
2022-04-06 13:23:15 +0530

    ui chnages done

--- src/app/selection-table/selection-table.component.ts
+++ src/app/selection-table/selection-table.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-selection-table',
-  templateUrl: './selection-table.component.html',
-  styleUrls: ['./selection-table.component.css']
+  selector: "app-selection-table",
+  templateUrl: "./selection-table.component.html",
+  styleUrls: ["./selection-table.component.scss"],
 })
 export class SelectionTableComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  openDialog() {
+    this.dialog.open(DialogElementsExampleDialog);
   }
 
+  ngOnInit(): void {}
 }
+@Component({
+  selector: "dialog-selection-table.component",
+  templateUrl: "dialog-selection-table.component.html",
+  styleUrls: ["./selection-table.component.scss"],
+})
+export class DialogElementsExampleDialog {}
